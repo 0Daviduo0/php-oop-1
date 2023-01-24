007f112ac3c421f2add97eef2cf0fb4c09e33760
Daviduo <davide0400@gmail.com>
2023-01-24 15:38:32 +0100

    added public function __construct

--- index.php
+++ index.php
@@ -7,7 +7,14 @@ class Movie {
     public $description;
     public $streamingPlatform;
 
+    public function __construct($length, $title, $description, $streamingPlatform){
 
+        $this->length = $length;
+        $this->title = $title;
+        $this->description = $description;
+        $this->streamingPlatform = $streamingPlatform;
+
+    }
 
 }
 
