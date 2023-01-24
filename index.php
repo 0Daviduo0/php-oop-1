f7229d0525b9078e981b3e8c428acf0979e1ae40
Daviduo <davide0400@gmail.com>
2023-01-24 15:50:01 +0100

    added function completeTitleInfo

--- index.php
+++ index.php
@@ -4,15 +4,25 @@ class Movie {
 
     public $length;
     public $title;
+    public $releaseYear;
+    public $genre;
     public $description;
     public $streamingPlatform;
 
-    public function __construct($length, $title, $description, $streamingPlatform){
+    public function __construct($length, $title, $releaseYear, $genre, $description, $streamingPlatform){
 
         $this->length = $length;
         $this->title = $title;
+        $this->releaseYear = $releaseYear;
+        $this->genre = $genre;
         $this->description = $description;
         $this->streamingPlatform = $streamingPlatform;
+
+    }
+
+    public function completeTitleInfo() {
+
+        return $this->title . " (" . $this->releaseYear . ")" . " - " . $this->genre;
 
     }
 
